297fb316b68ba8f707a761a0e039b522930af226
Sushma Sree <119730289+sushmasree2026@users.noreply.github.com>
2023-05-18 18:52:06 +0530

    #include<stdio.h> int main() {     int n,i,a=0,m=1;     scanf("%d",&n);     for(i=1;n>0;i++)     {         i=n%10;         n=n/10;         a=a+i;         m=m*i;     }     if(a==m)     {         printf("Spy Number");     }     else     {         printf("Not Spy Number");     } }

--- Spy_Number.c
+++ Spy_Number.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,s=0,m=1;
-    scanf("%d",&n);
+    int n,i,a=0,m=1;
+    scanf("%d",&n); 
     for(i=1;n>0;i++)
     {
         i=n%10;
         n=n/10;
-        s=s+i;
+        a=a+i;
         m=m*i;
     }
-    if(m==s)
+    if(a==m)
     {
         printf("Spy Number");
     }
