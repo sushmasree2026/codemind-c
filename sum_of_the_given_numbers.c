b21f4231221b5961a1ec14bce39fba68fdfecf09
Sushma Sree <119730289+sushmasree2026@users.noreply.github.com>
2023-05-24 14:53:51 +0530

    #include<stdio.h> int main() {     int i, j, rows, columns, a[10][10], Sum;     scanf("%d", &i);     for(rows = 0; rows < i; rows++)     {         for(columns = 0; columns < 2; columns++)         {             scanf("%d", &a[rows][columns]);         }     }     for(rows = 0; rows < i; rows++)     {         Sum = 0;         for(columns = 0; columns < 2; columns++)         {             Sum = Sum + a[rows][columns];         }         printf("%d ",Sum);     }     return 0; }

--- sum_of_the_given_numbers.c
+++ sum_of_the_given_numbers.c
@@ -1,11 +1,24 @@
-#include<stdio.h>
+#include<stdio.h> 
 int main()
 {
-    int n,i,arr[100],a,b;
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-    { scanf("%d %d",&a,&b);
-      printf("%d
-",a+b);
+    int i, j, rows, columns, a[10][10], Sum;
+    scanf("%d", &i); 
+    for(rows = 0; rows < i; rows++) 
+    {
+        for(columns = 0; columns < 2; columns++)
+        {
+            scanf("%d", &a[rows][columns]);
+        }
     }
+    for(rows = 0; rows < i; rows++) 
+    {
+        Sum = 0; 
+        for(columns = 0; columns < 2; columns++) 
+        {
+            Sum = Sum + a[rows][columns];
+        }
+        printf("%d
+",Sum);
+    }
+    return 0;
 }
