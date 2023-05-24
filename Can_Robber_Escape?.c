337618dc858bf8327affd49740250fb4213e1236
Sushma Sree <119730289+sushmasree2026@users.noreply.github.com>
2023-05-24 11:56:07 +0530

    #include<stdio.h> int main() {     int n,x[20],odd=0,i;     scanf("%d",&n);     for(i=0;i<n;i++) scanf("%d",&x[i]);     for(i=0;i<n;i++)     {         if(x[i]%2!=0) odd++;     }     if(odd<=2) printf("YES");     else printf("NO");     return 0; }

--- Can_Robber_Escape?.c
+++ Can_Robber_Escape?.c
@@ -1,24 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int n,x[20],odd=0,i;
-    scanf("%d",&n);
+    int n,x[20],odd=0,i; 
+    scanf("%d",&n); 
+    for(i=0;i<n;i++) scanf("%d",&x[i]); 
     for(i=0;i<n;i++)
     {
-       scanf("%d",&x[i]);
+        if(x[i]%2!=0) odd++;
     }
-    for(i=0;i<n;i++)
-    {
-        if(x[i]%2!=0)
-        odd++;
-    }
-    if(odd<=2) 
-    {
-       printf("YES");
-    }
-    else
-    {
-       printf("NO");
-    }
+    if(odd<=2) printf("YES"); 
+    else printf("NO"); 
     return 0;
 }
