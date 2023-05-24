1b7a3b566aa0a562389eeecefdbd02d53418bb38
Sushma Sree <119730289+sushmasree2026@users.noreply.github.com>
2023-05-24 16:38:12 +0530

    #include<stdio.h> int main(){     int n,scales[10],i,j,found=0,min=9999;     scanf("%d",&n);     for(i=0;i<n;i++) scanf("%d",&scales[i]);     for(i=0;i<n;i++)     {         if(min>scales[i]) min=scales[i];     }     for(i=min;i>=1;i--)     {         found=0;         for(j=0;j<n;j++)         {             if(scales[j]%i!=0)             {                 found=1;                 break;             }         }         if(found==0)         {             printf("%d",i);             break;         }     }     return 0; }

--- Minimum_Scale_length.c
+++ Minimum_Scale_length.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
-int main()
-{
-    int n,scales[10],i,j,found=0,min=9999; 
-    scanf("%d",&n); 
-    for(i=0;i<n;i++) scanf("%d",&scales[i]); 
+int main(){ 
+    int n,scales[10],i,j,found=0,min=9999;
+    scanf("%d",&n);
+    for(i=0;i<n;i++) scanf("%d",&scales[i]);
     for(i=0;i<n;i++)
     {
         if(min>scales[i]) min=scales[i];
@@ -13,7 +12,7 @@ int main()
         found=0;
         for(j=0;j<n;j++)
         {
-            if(scales[j]%i!=0) 
+            if(scales[j]%i!=0)
             {
                 found=1;
                 break;
